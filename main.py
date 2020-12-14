0d09aeb5bfa220ce580ca226bbd3faafa433c697
David F <Skilliciousboy@icloud.com>
2020-12-14 21:03:45 +0000

    randomised type speed

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from split import split
 from pynput.keyboard import Key, Controller
 import pyautogui
 import time
+import random
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Dell\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 keyboard = Controller()
 
 print("Initiating...")
-time.sleep(5)
-
-# 1003 656
+time.sleep(2)
 
 if pyautogui.pixel(999, 661)[1] == 208: 
+    tries = 0
     while kb.is_pressed('esc') == False:
+        sleepTime = random.uniform(0.01, 0.2)
         img = pyautogui.screenshot(region=(952, 706, 500, 500))
         #img.save(r"currentLetter.png")
 
@@ -29,22 +30,15 @@ if pyautogui.pixel(999, 661)[1] == 208:
         if lettersArray[0] == '\x0c':
             keyboard.press(Key.space)
             keyboard.release(Key.space)
-            time.sleep(0.2)
+            time.sleep(sleepTime)
         elif lettersArray[1] == ' ':
             keyboard.press(key)
             keyboard.release(key)
-            time.sleep(0.2)
+            time.sleep(sleepTime)
             keyboard.press(Key.space)
             keyboard.release(Key.space)
-            time.sleep(0.2)
+            time.sleep(sleepTime)
         else:
             keyboard.press(key)
             keyboard.release(key)
-            time.sleep(0.2)
-
-
-
-
-
-
-
+            time.sleep(sleepTime)
