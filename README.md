652ea13d7aca8856997e6d9b8541268735de5a80
David F <Skilliciousboy@icloud.com>
2020-12-15 10:23:07 +0000

    after 3 attempts of a letter it moves on, checks the timer is 60 before starting, new imgs dir

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AutoKB
 <p align="center"> 
-<img src="preview.png">
+<img src="./imgs/preview.png">
 </p>
 
 [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
--- main.py
+++ main.py
@@ -8,16 +8,59 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Dell\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 keyboard = Controller()
+mouse = Controller()
 
 print("Initiating...")
+canStart = False
 time.sleep(2)
 
-if pyautogui.pixel(999, 661)[1] == 208: 
-    tries = 0
+def pressKey(keyToPress):
+    keyboard.press(keyToPress)
+    keyboard.release(keyToPress)
+    sleepTime = random.uniform(0.01, 0.2)
+    time.sleep(sleepTime)
+    
+def ifSame(listOfStrings):
+    result = False
+    if len(listOfStrings) > 0 :
+        result = all(elem == listOfStrings[0] for elem in listOfStrings)
+        
+    if result :
+        return True
+    else:        
+        return False
+    
+start = pyautogui.locateCenterOnScreen('./imgs/retry.png')
+pyautogui.moveTo(start)
+pyautogui.click(pyautogui.position())
+time.sleep(0.2)
+
+#693, 450
+
+timer = pyautogui.screenshot(region=(688, 450, 80, 65))
+timer.save(r"thing.png")
+timerTime = pytesseract.image_to_string(timer, config='tessedit_char_whitelist=0123456789')
+timerTimeArray = split(timerTime)
+print(timerTimeArray)
+
+if timerTimeArray[0] == "6" and timerTimeArray[1] == "0":
+    canStart = True
+else:
+    canStart = False
+
+if canStart: 
+    isEnough = False
+    lastKey = ["wrhj80h8v", "ksodfjpsdofs", "jr09hf340uithg"]
+    
     while kb.is_pressed('esc') == False:
-        sleepTime = random.uniform(0.01, 0.2)
+        time.sleep(0.2)
         img = pyautogui.screenshot(region=(952, 706, 500, 500))
         #img.save(r"currentLetter.png")
+        print(lastKey)
+        areTheSame = ifSame(lastKey)
+        
+        if areTheSame == True:
+            pressKey(letters[-1])
 
         letters = pytesseract.image_to_string(img, config='tessedit_char_whitelist=0123456789')
         #print(letters)
@@ -25,20 +68,38 @@ if pyautogui.pixel(999, 661)[1] == 208:
         #print(lettersArray)
         key = lettersArray[0].lower()
         isSpace = letters[1]
-        print(key, isSpace)
+        #print(key, isSpace)
 
         if lettersArray[0] == '\x0c':
-            keyboard.press(Key.space)
-            keyboard.release(Key.space)
-            time.sleep(sleepTime)
+            pressKey(Key.space)
         elif lettersArray[1] == ' ':
-            keyboard.press(key)
-            keyboard.release(key)
-            time.sleep(sleepTime)
-            keyboard.press(Key.space)
-            keyboard.release(Key.space)
-            time.sleep(sleepTime)
+            if len(lastKey) > 3:
+                check = ifSame(lastKey)
+                if check:
+                    print("y")
+                else:
+                    print("n")
+                    lastKey.insert(0, key)
+                    pressKey(key)
+                    pressKey(Key.space)
+                    del lastKey[-1:]
+            else:
+                lastKey.insert(0, key)
+                pressKey(key)
+                pressKey(Key.space)
+                del lastKey[-1:]
         else:
-            keyboard.press(key)
-            keyboard.release(key)
-            time.sleep(sleepTime)
+            if len(lastKey) > 3:
+                del lastKey[-1:]
+                check = ifSame(lastKey)
+                if check:
+                    print("y")
+                else:
+                    print("n")
+                    lastKey.insert(0, key)
+                    pressKey(key)
+                    del lastKey[-1:]
+            else:
+                lastKey.insert(0, key)
+                pressKey(key)
+                del lastKey[-1:]
